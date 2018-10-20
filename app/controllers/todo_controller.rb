dfb7ef8c96480de4f548f1dd48588cf85a5dd221
Sam Hunt <samhunt5557@gmail.com>
2018-10-20 19:55:44 +0000

    Day 3 end

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,8 +1,29 @@
 class TodoController < ApplicationController
+
   def show
-    @todo_description = "Lets go some math"
+    todo_id= params[:id]
+    @todo_description = 
     @todo_pomodoro_estimate = 4
+    if todo_id=='1' 
+      @todo_description = "Make the curriculum"
+      @todo_pomodoro_estimate = 4
+    elsif todo'id'
+    if todo_id=='2'  
+      @todo_description = "Make the curriculum"
+      @todo_pomodoro_estimate = 4
+      if todo_id=='3'
+        @todo_description = "Make the curriculum"
+      @todo_pomodoro_estimate = 4
+      if todo_id=='4'
+      @todo_description = "Make the curriculum"
+      @todo_pomodoro_estimate = 4
+      end
+      end
+    end
+    end
   end
+  
+  
   
   def index
   end
