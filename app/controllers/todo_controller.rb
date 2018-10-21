f68d975cebc96f20b46936898d93332fc984101c
Sam Hunt <samhunt5557@gmail.com>
2018-10-21 15:50:10 +0000

    added things to database

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,26 +1,10 @@
 class TodoController < ApplicationController
 
   def show
-    todo_id= params[:id]
-    @todo_description = 
-    @todo_pomodoro_estimate = 4
-    if todo_id=='1' 
-      @todo_description = "Make the curriculum"
-      @todo_pomodoro_estimate = 4
-    elsif todo'id'
-    if todo_id=='2'  
-      @todo_description = "Make the curriculum"
-      @todo_pomodoro_estimate = 4
-      if todo_id=='3'
-        @todo_description = "Make the curriculum"
-      @todo_pomodoro_estimate = 4
-      if todo_id=='4'
-      @todo_description = "Make the curriculum"
-      @todo_pomodoro_estimate = 4
-      end
-      end
-    end
-    end
+   
+    
+    @todo = Todo.find_by_id(params[:id])
+
   end
   
   
